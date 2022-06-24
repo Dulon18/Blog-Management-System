51bd272c0fb29bda101779c0cfe32c284ce8dc4f
Dulon18 <dulon587@gmail.com>
2022-06-24 22:43:17 +0600

    add admin templete

---
+++ resources/views/admin/fixed/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div class="sidebar-menu sticky-sidebar-menu">
+
+<!-- logo start -->
+<div class="logo">
+  <h1><a href="index.html">BMS</a></h1>
+</div>
+
+<div class="logo-icon text-center">
+  <a href="index.html" title="logo"><img src="{{url('Backend/assets/images/logo.png')}}" alt="logo-icon"> </a>
+</div>
+<!-- //logo end -->
+
+<div class="sidebar-menu-inner">
+
+  <!-- sidebar nav start -->
+  <ul class="nav nav-pills nav-stacked custom-nav">
+    <li class="active"><a href="index.html"><i class="fa fa-tachometer"></i><span> Dashboard</span></a>
+    </li>
+    <li class="menu-list">
+      <a href="#"><i class="fa fa-cogs"></i>
+        <span>Elements <i class="lnr lnr-chevron-right"></i></span></a>
+      <ul class="sub-menu-list">
+        <li><a href="carousels.html">Carousels</a> </li>
+        <li><a href="cards.html">Default cards</a> </li>
+        <li><a href="people.html">People cards</a></li>
+      </ul>
+    </li>
+    <li><a href="pricing.html"><i class="fa fa-table"></i> <span>Pricing tables</span></a></li>
+    <li><a href="blocks.html"><i class="fa fa-th"></i> <span>Content blocks</span></a></li>
+    <li><a href="forms.html"><i class="fa fa-file-text"></i> <span>Forms</span></a></li>
+  </ul>
+  <!-- //sidebar nav end -->
+  <!-- toggle button start -->
+  <a class="toggle-btn">
+    <i class="fa fa-angle-double-left menu-collapsed__left"><span>Collapse Sidebar</span></i>
+    <i class="fa fa-angle-double-right menu-collapsed__right"></i>
+  </a>
+  <!-- //toggle button end -->
+</div>
+</div>
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>BMS</title>
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{url('Backend/assets/css/style-starter.css')}}">
+
+  <!-- google fonts -->
+  <link href="//fonts.googleapis.com/css?family=Nunito:300,400,600,700,800,900&display=swap" rel="stylesheet">
+</head>
+
+<body class="sidebar-menu-collapsed">
+  <div class="se-pre-con"></div>
+<section>
+  <!-- sidebar menu start -->
+@include('admin.fixed.sidebar')
+  <!-- //sidebar menu end -->
+  <!-- header-starts -->
+@include('admin.fixed.nav')
+  <!-- main content start -->
+<div class="main-content">
+
+  <!-- content -->
+  <div class="container-fluid content-top-gap">
+
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb my-breadcrumb">
+        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+      </ol>
+    </nav>
+
+
+    <!-- statistics data -->
+    <div class="statistics">
+      <div class="row">
+        <div class="col-xl-6 pr-xl-2">
+          <div class="row">
+            <div class="col-sm-6 pr-sm-2 statistics-grid">
+              <div class="card card_border border-primary-top p-4">
+                <i class="lnr lnr-users"> </i>
+                <h3 class="text-primary number">29.75 M</h3>
+                <p class="stat-text">Total Users</p>
+              </div>
+            </div>
+            <div class="col-sm-6 pl-sm-2 statistics-grid">
+              <div class="card card_border border-primary-top p-4">
+                <i class="lnr lnr-eye"> </i>
+                <h3 class="text-secondary number">51.25 K</h3>
+                <p class="stat-text">Daily Visitors</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-6 pl-xl-2">
+          <div class="row">
+            <div class="col-sm-6 pr-sm-2 statistics-grid">
+              <div class="card card_border border-primary-top p-4">
+                <i class="lnr lnr-cloud-download"> </i>
+                <h3 class="text-success number">166.89 M</h3>
+                <p class="stat-text">Downloads</p>
+              </div>
+            </div>
+            <div class="col-sm-6 pl-sm-2 statistics-grid">
+              <div class="card card_border border-primary-top p-4">
+                <i class="lnr lnr-cart"> </i>
+                <h3 class="text-danger number">1,250k</h3>
+                <p class="stat-text">Purchased</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //statistics data -->
+
+    <!-- charts -->
+    <div class="chart">
+      <div class="row">
+        <div class="col-lg-6 pr-lg-2 chart-grid">
+          <div class="card text-center card_border">
+            <div class="card-header chart-grid__header">
+              Bar Chart
+            </div>
+            <div class="card-body">
+              <!-- bar chart -->
+              <div id="container">
+                <canvas id="barchart"></canvas>
+              </div>
+              <!-- //bar chart -->
+            </div>
+            <div class="card-footer text-muted chart-grid__footer">
+              Updated 2 hours ago
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 pl-lg-2 chart-grid">
+          <div class="card text-center card_border">
+            <div class="card-header chart-grid__header">
+              Line Chart
+            </div>
+            <div class="card-body">
+              <!-- line chart -->
+              <div id="container">
+                <canvas id="linechart"></canvas>
+              </div>
+              <!-- //line chart -->
+            </div>
+            <div class="card-footer text-muted chart-grid__footer">
+              Updated just now
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //charts -->
+
+   
+
+  </div>
+  <!-- //content -->
+</div>
+<!-- main content end-->
+</section>
+  <!--footer section start-->
+<footer class="dashboard">
+  <p>&copy 2022 BMS. All Rights Reserved | Design by <a href="/" target="_blank"
+      class="text-primary">W3layouts.</a></p>
+</footer>
+<!--footer section end-->
+<!-- move top -->
+<button onclick="topFunction()" id="movetop" class="bg-primary" title="Go to top">
+  <span class="fa fa-angle-up"></span>
+</button>
+<script>
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction()
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("movetop").style.display = "block";
+    } else {
+      document.getElementById("movetop").style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+</script>
+<!-- /move top -->
+
+
+<script src="{{url('Backend/assets/js/jquery-3.3.1.min.js')}}"></script>
+<script src="{{url('Backend/assets/js/jquery-1.10.2.min.js')}}"></script>
+
+<!-- chart js -->
+<script src="{{url('Backend/assets/js/Chart.min.js')}}"></script>
+<script src="{{url('Backend/assets/js/utils.js')}}"></script>
+<!-- //chart js -->
+
+<!-- Different scripts of charts.  Ex.Barchart, Linechart -->
+<script src="{{url('Backend/assets/js/bar.js')}}"></script>
+<script src="{{url('Backend/assets/js/linechart.js')}}"></script>
+<!-- //Different scripts of charts.  Ex.Barchart, Linechart -->
+
+
+<script src="{{url('Backend/assets/js/jquery.nicescroll.js')}}"></script>
+<script src="{{url('Backend/assets/js/scripts.js')}}"></script>
+
+<!-- close script -->
+<script>
+  var closebtns = document.getElementsByClassName("close-grid");
+  var i;
+
+  for (i = 0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function () {
+      this.parentElement.style.display = 'none';
+    });
+  }
+</script>
+<!-- //close script -->
+
+<!-- disable body scroll when navbar is in active -->
+<script>
+  $(function () {
+    $('.sidebar-menu-collapsed').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+</script>
+<!-- disable body scroll when navbar is in active -->
+
+ <!-- loading-gif Js -->
+ <script src="{{url('Backend/assets/js/modernizr.js')}}"></script>
+ <script>
+     $(window).load(function () {
+         // Animate loader off screen
+         $(".se-pre-con").fadeOut("slow");;
+     });
+ </script>
+ <!--// loading-gif Js -->
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{url('Backend/assets/js/bootstrap.min.js')}}"></script>
+
+</body>
+
+</html>
+  
